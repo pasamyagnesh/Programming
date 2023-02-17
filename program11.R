462311f2d4af5fedaf4623fb60f3230f88d5bef0
Yagnesh Pasam <93670699+yagnesh2004@users.noreply.github.com>
2023-02-17 11:11:10 +0530

    Add files via upload
    R-lab progaramns on February-17-2023

---
+++ program11.R
@@ -0,0 +1,6 @@
+a<-c(3,3,3)
+b<-c(2,2,2)
+c<-c(1,1,1)
+thismatrix <- matrix( c(a,b,c), nrow = 3, ncol = 3)
+
+print(thismatrix)
---
+++ program_unique.R
@@ -0,0 +1,8 @@
+str1 =c("the","ame","yagnesh","the","yagnesh")
+print(str1)
+print("Unique elements of the said vector:")
+print(unique(tolower(str1)))
+nums = c(1, 2, 2, 3, 4, 4, 5, 6)
+print(nums)
+print("Unique elements of the said vector:")
+print(unique(nums))
